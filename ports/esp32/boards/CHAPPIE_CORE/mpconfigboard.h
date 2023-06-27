aeadcfea30d3402da8f2f0f1de6a4be6650fa4c3
zhyolo <zhouyong0706@outlook.com>
2023-06-27 09:59:38 +0800

    添加 Chappie core

---
+++ ports/esp32/boards/CHAPPIE_CORE/mpconfigboard.h
@@ -0,0 +1,8 @@
+#define MICROPY_HW_BOARD_NAME               "Chappie core"
+#define MICROPY_HW_MCU_NAME                 "ESP32S3"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "ChappieCore"
+
+#define MICROPY_PY_MACHINE_DAC              (0)
+
+#define MICROPY_HW_I2C0_SCL                 (12)
+#define MICROPY_HW_I2C0_SDA                 (11)
